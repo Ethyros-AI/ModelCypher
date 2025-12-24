6aa684d6c97c994266db63be42ccc00a8d955c5e
anon57396 <jasonkempf@gmail.com>
2025-12-24 15:37:29 -0600

    Add copyright notices to test files. Knowledge should be free. Always.
    This commit adds the GNU Affero General Public License copyright notice to all files in the repository. The updates ensure compliance with licensing requirements and clarify the terms under which the ModelCypher software can be used and modified.

--- src/modelcypher/core/domain/entropy/sep_probe.py
+++ src/modelcypher/core/domain/entropy/sep_probe.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2025 EthyrosAI LLC / Jason Kempf
+#
+# This file is part of ModelCypher.
+#
+# ModelCypher is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ModelCypher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with ModelCypher.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 Semantic Entropy Probe (SEP) for Fast Entropy Prediction.
 
--- src/modelcypher/infrastructure/__init__.py
+++ src/modelcypher/infrastructure/__init__.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2025 EthyrosAI LLC / Jason Kempf
+#
+# This file is part of ModelCypher.
+#
+# ModelCypher is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ModelCypher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with ModelCypher.  If not, see <https://www.gnu.org/licenses/>.
+
 """Infrastructure layer for dependency injection.
 
 This module provides the composition root for wiring all adapters to ports.
