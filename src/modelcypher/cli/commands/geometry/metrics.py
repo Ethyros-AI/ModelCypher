1a98242831487c6af5c1cfcc0a86be2604660757
anon57396 <jasonkempf@gmail.com>
2026-02-04 14:38:30 -0600

    refactor: reorganize geometry diagnostic imports and move lora diagnostic service to a new file

--- src/modelcypher/cli/commands/geometry/metrics.py
+++ src/modelcypher/cli/commands/geometry/metrics.py
@@ -468,7 +468,7 @@ def geometry_metrics_lora_diagnostic(
     if layers:
         target_layers = [int(x.strip()) for x in layers.split(",")]
 
-    from modelcypher.adapters.geometry.lora_geometry_diagnostic import (
+    from modelcypher.core.use_cases.lora_diagnostic_service import (
         run_diagnostic,
     )
 
